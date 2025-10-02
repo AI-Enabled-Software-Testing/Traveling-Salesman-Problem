736a48644c1c081442314d361405f1811620d30b
Kelvin Mock <kelvin.mock@gmail.com>
2025-10-02 12:51:51 -0400

    Removed unused codes

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import pandas as pd # for csv output
 
-# Algorithms
-from algorithm.nearest_neighbor import NearestNeighbor
-from algorithm.simulated_annealing import SimulatedAnnealing
-from algorithm.genetic_algo import GeneticAlgorithmSolver
-from algorithm.random_solver import RandomSolver
-
 # Utilities
 from pathlib import Path
 import time
